b75115598cdd507dcd74fac936442934703f0904
JohnOdhiambo <john.oduor.j@gmail.com>
2023-04-08 22:32:01 +0300

    Generating pdf from html

---
+++ SamplePdfGenerator/SamplePdfGenerator/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SamplePdfGenerator.Models;
+
+namespace SamplePdfGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Chart()
+        {
+            return View();
+        }
+        //public JsonResult ExportReport(string imageData)
+        //{
+        //    string fileName = Path.Combine(Server.MapPath("~/ExportImage"), DateTime.Now.ToString("ddMMyyyyhhmmsstt") + ".png");
+        //    using (FileStream fs = new FileStream(fileName, FileMode.Create))
+        //    {
+        //        using (BinaryWriter bw = new BinaryWriter(fs))
+        //        {
+        //            byte[] data = Convert.FromBase64String(imageData);
+        //            bw.Write(data);
+        //            bw.Close();
+        //        }
+        //    }
+        //    return new JsonResult { Data = "Image saved successfully" };
+        //}
+        //public ActionResult Report()
+        //{
+        //    SelectPdf.GlobalProperties.HtmlEngineFullPath = HttpContext.ApplicationInstance.Server.MapPath("~/App_Data/Select.Html.dep");
+        //    try
+        //    {
+        //        string pdf_page_size = "A4";
+        //        PdfPageSize pageSize = (PdfPageSize)Enum.Parse(typeof(PdfPageSize),
+        //            pdf_page_size, true);
+
+        //        string pdf_orientation = "Portrait";
+        //        PdfPageOrientation pdfOrientation =
+        //            (PdfPageOrientation)Enum.Parse(typeof(PdfPageOrientation),
+        //                pdf_orientation, true);
+
+        //        int webPageWidth = 1600;
+
+        //        int webPageHeight = 0;
+
+        //        // instantiate a html to pdf converter object
+        //        HtmlToPdf converter = new HtmlToPdf();
+
+        //        // set converter options
+        //        converter.Options.PdfPageSize = pageSize;
+        //        converter.Options.PdfPageOrientation = pdfOrientation;
+        //        converter.Options.WebPageWidth = webPageWidth;
+        //        converter.Options.WebPageHeight = webPageHeight;
+
+        //        // create a new pdf document converting an url
+        //        PdfDocument doc =
+        //            converter.ConvertUrl(string.Format("http://{0}:{1}/Home/Index", HttpContext.Request.Url.Host,
+        //                HttpContext.Request.Url.Port));
+
+        //        // save pdf document
+        //        doc.Save(HttpContext.ApplicationInstance.Response, false, "Sample.pdf");
+
+        //        // close pdf document
+        //        doc.Close();
+
+        //        return Content("");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return (e);
+        //    }
+
+        //}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SamplePdfGenerator/SamplePdfGenerator/Controllers/PdfController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SamplePdfGenerator.Controllers
+{
+    public class PdfController : Controller
+    {
+        public ActionResult Index()
+        {
+            byte[] bytes = PopulateChart();
+            ViewBag.ChartImageUrl = "data:image/png;base64," + Convert.ToBase64String(bytes, 0, bytes.Length);
+            return View();
+        }
+
+        [HttpPost]
+        public FileResult Export()
+        {
+            byte[] bytes = PopulateChart();
+            ViewBag.ChartImageUrl = "data:image/png;base64," + Convert.ToBase64String(bytes, 0, bytes.Length);
+            using (MemoryStream stream = new System.IO.MemoryStream())
+            {
+                //Initialize the PDF document object.
+                using (Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 10f))
+                {
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                    pdfDoc.Open();
+
+                    //Add the Image file to the PDF document object.
+                    iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(bytes);
+                    pdfDoc.Add(img);
+                    pdfDoc.Close();
+
+                    //Download the PDF file.
+                    return File(stream.ToArray(), "application/pdf", "Chart.pdf");
+                }
+            }
+        }
+
+        private static byte[] PopulateChart()
+        {
+            string query = "SELECT TOP 5 ShipCity, COUNT(orderid) TotalOrders";
+            query += " FROM Orders WHERE ShipCountry = 'USA' GROUP BY ShipCity";
+            string constr = ConfigurationManager.ConnectionStrings["Constring"].ConnectionString;
+            List<OrderModel> chartData = new List<OrderModel>();
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            chartData.Add(new OrderModel
+                            {
+                                ShipCity = sdr["ShipCity"].ToString(),
+                                TotalOrders = Convert.ToInt32(sdr["TotalOrders"])
+                            });
+                        }
+                    }
+
+                    con.Close();
+                }
+            }
+
+            Chart chart = new Chart(width: 500, height: 500, theme: ChartTheme.Blue);
+            chart.AddTitle("USA City Distribution");
+            chart.AddSeries("Default", chartType: "Pie", xValue: chartData, xField: "ShipCity", yValues: chartData, yFields: "TotalOrders");
+            return chart.GetBytes(format: "jpeg");
+        }
+
+    }
+}
